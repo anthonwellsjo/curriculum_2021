1e5db3d8a737b19e06c59b9a4f5bf1e5d0a2ce37
AnthonWellsjo <anthonwellsjo@gmail.com>
2021-07-14 23:19:27 +0200

    yeah

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -120,31 +120,11 @@ export default function Home({ projects, tech }: props) {
   }
 
 
-
-  const title = "Anthon Wellsjö Portfolio";
-  const description = "Curriculum 2021 for Carl Anthon Wellsjö, swedish web developer, working remote from Perugia, Italy.";
-
   return (
 
     <div ref={focusMe} onClick={onClickEventHandler} style={{ width: "100vw", height: "100vh", overflow: "hidden", cursor: "pointer", overflowY: "hidden" }}>
       <Head>
-        <meta name="twitter:card" content="Curriculum/portfolio web site 2021 for Carl Anthon Wellsjö, swedish web developer, working remote from Perugia, Italy." />
-        <meta name="twitter:site" content="@Anthon_Wellsjo" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="https://pbs.twimg.com/profile_images/778947258117152768/LjuuB1T8_400x400.jpg" />
-
-        <meta property="og:url" content={"https://curriculum-2021.vercel.app/"} key="ogurl" />
-        <meta property="og:image" content={"https://pbs.twimg.com/profile_images/778947258117152768/LjuuB1T8_400x400.jpg"} key="ogimage" />
-        <meta property="og:site_name" content={title} key="ogsitename" />
-        <meta name="title" property="og:title" content={"main"} key="ogtitle" />
-        <meta property="og:description" content={description} key="ogdesc" />
-
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta name="HandheldFriendly" content="true" />
-        <link rel="icon" href="/laptop.png" />
+        
       </Head>
       <Background />
 

14009eaa05e76b006e5acae7766201ff133ed646
AnthonWellsjo <anthonwellsjo@gmail.com>
2021-10-08 13:14:31 +0200

    probably breaking stuff

--- client/src/components/bio/Bio.tsx
+++ client/src/components/bio/Bio.tsx
@@ -70,12 +70,12 @@ const Bio: React.FC = () => {
     <div onClick={onClickEventHandler} style={{ width: "100%", height: "100%", position: "absolute", display: "flex", alignItems: "center", justifyContent: "center", userSelect: "none", overflowY: "scroll", top: "0", zIndex: 0, backdropFilter: "blur(5px)", backgroundColor: "rgba(256,256,256,0.3)" }}>
       <animated.div className="noScrollBar" style={{ ...styles, ...animStyle, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-evenly", overflowY: "scroll" }}>
         {showContent &&
-            <animated.div style={{ ...textStyle, textAlign: "justify", borderRadius: "5px", width: "80%", marginTop: viewWidth < 750 ? "-20px" : "50px" }}>
-              <p className={classes.bodyText} style={{ fontFamily: "'Archivo Black', sans-serif", fontSize: "1.1em", marginBottom: "0" }}>Hey,</p>
-              <p className={classes.bodyText}>My name is Anthon and I'm a Swedish <strong>full-stack web developer</strong> based in Perugia, Italy with my wife and three kids.</p>
-              <p className={classes.bodyText}><strong>I speak four languages </strong> fluently (Swedish, French, Italian, and English) and other than passionately developing websites, I love garden work, kite surfing, and playing the guitar.</p>
-              <p className={classes.bodyText}>If you consider hiring me, then you can count on <strong>an effective and open minded coworker</strong>. I love learning, and arriving at the best solutions, no matter who had the idea.</p>
-            </animated.div>
+          <animated.div style={{ ...textStyle, textAlign: "justify", borderRadius: "5px", width: "80%", marginTop: viewWidth < 750 ? "-20px" : "50px" }}>
+            <p className={classes.bodyText} style={{ fontFamily: "'Archivo Black', sans-serif", fontSize: "1.1em", marginBottom: "0" }}>Hey,</p>
+            <p className={classes.bodyText}>My name is Anthon and I'm a Swedish <strong>full-stack web developer</strong> based in Perugia, Italy with my wife and three kids.</p>
+            <p className={classes.bodyText}><strong>I speak four languages </strong> fluently (Swedish, French, Italian, and English) and other than passionately developing websites, I love garden work, kite surfing, and playing the guitar.</p>
+            <p className={classes.bodyText}>If you consider hiring me, then you can count on <strong>an effective and open minded coworker</strong>. I love learning, and arriving at the best solutions, no matter who had the idea.</p>
+          </animated.div>
         }
         {showContent && viewHeight > 600 && viewWidth > 339 &&
           <animated.div style={{ ...hobbyStyle, display: "flex", width: "100%", justifyContent: "center", position: "absolute", bottom: "40px" }}>
